c4737b2295ce6ed3a698c6693b4ed1f5ba74ee06
chahyeonho <chamym@naver.com>
2021-03-28 18:49:34 +0900

    mustache index , controller add

--- src/main/java/com/hostar/education/springboot/service/posts/PostsService.java
+++ src/main/java/com/hostar/education/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hostar.education.springboot.service.posts;
 
 import com.hostar.education.springboot.domain.posts.Posts;
 import com.hostar.education.springboot.domain.posts.PostsRepository;
+import com.hostar.education.springboot.web.dto.PostsListResponseDto;
 import com.hostar.education.springboot.web.dto.PostsResponseDto;
 import com.hostar.education.springboot.web.dto.PostsSaveRequestDto;
 import com.hostar.education.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,13 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly ==> 트랙잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선 :: 등록, 수정, 삭제 기능이 전혀없는 서비스 메소드에서 사용하는걸 추천
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // :: 참조 메소드
+//                .map(p -> new PostsListResponseDto(p)) // 위와 동일한듯
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/hostar/education/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/hostar/education/springboot/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -41,6 +42,28 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_save() throws Exception {
+        LocalDateTime now  = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> createDate=" + posts.getCreateDate());
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>> modifiedDate=" + posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreateDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
 
