c977f519664541c0467b43fa6e265502e15bad65
chahyeonho <chamym@naver.com>
2021-03-31 23:37:53 +0900

    Today OAuth2 SessionUser, CustomOAuth2UserService, OAuthAttributes add

--- src/main/java/com/hostar/education/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/hostar/education/springboot/config/auth/CustomOAuth2UserService.java
@@ -30,7 +30,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         /**
          * 현재 로그인 진행 중인 서비스를 구분하는 코드
-         * 지금은 구굴만 사용하는 불필요한 값이지만, 이후 네이버 로그인 연동 시에 네이버 로그인인지, 구글 로그인인지 구분하기 위해 사용
+         * 지금은 구글만 사용하는 불필요한 값이지만, 이후 네이버 로그인 연동 시에 네이버 로그인인지, 구글 로그인인지 구분하기 위해 사용
          */
         String registraionId = userRequest.getClientRegistration().getClientId();
 
@@ -53,10 +53,16 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
          * SessionUser
          * 세션에 사용자 정보를 저장하기 위한 Dto 클래스
          * SeesionUser에는 인증된 사용자 정보만 필요하다.그 외 필요한 정보들은 필요없으니 name, email, picture만 필드로 선언한다.
+         *
+         * 세션에 직접 User객체를 구현하지 않는 이유는 User클래스에 직렬화를 구현하지 않으면 에러가 난다.
+         * 그렇다면 User클래스에 직렬화를 구현하면 해결될 것인가?
+         * User클래스는 엔티티이기 때문에 @OneToMany, @ManyToMany 등의 자식 엔티티를 언제 갖게될지 모르므로 성능 이슈, 부수 효과가 발생활 확률이 높다.
+         * 따라서 직렬화 기능을 가진 세션 Dto를 생성하면 이후 운영 및 유지보때 많은 도움이 된다.
+         *
          */
         httpSession.setAttribute("user", new SessionUser(user));
 
-        return new DefaultOAuth2User(Collections.singleton(
+        return new DefaultOAuth2User(Collections.singleton(  // singleton => 단일 원소, 원소가 딱 1개일 경우에 사용한다.
                 new SimpleGrantedAuthority(user.getRoleKey())),
                 attributes.getAttributes(), attributes.getNameAttributeKey());
     }
